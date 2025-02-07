9744cfa0d00844ce3ed74e5907740f44bab05186
giovannifoletto <giovanni.foletto@gmail.com>
2025-02-07 01:04:37 +0100

    process handling complete

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <fcntl.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +8,7 @@
 #include <sys/shm.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
 
 #define NUM_SEGMENTS 16
@@ -14,6 +17,8 @@
 
 #define SHM_KEY 1234 // Shared memory key
 #define MSG_KEY 5678 // Message queue key
+
+sem_t *track_semaphores[NUM_SEGMENTS];
 
 // Train data structure
 typedef struct {
@@ -28,6 +33,8 @@ struct msg_buffer {
   long msg_type;
   int itinerary[NUM_SEGMENTS];
   int num_steps;
+  int start;
+  int dest;
 };
 
 void load_routes(Train *);
@@ -54,6 +61,21 @@ int *init_shared_memory() {
   return shm_ptr;
 }
 
+// Function to initialize POSIX semaphores
+void init_semaphores() {
+  for (int i = 0; i < NUM_SEGMENTS; i++) {
+    char sem_name[20];
+    snprintf(sem_name, sizeof(sem_name), "/track_sem_%d", i);
+    sem_unlink(sem_name); // Ensure no previous instance exists
+    // sem_open(sem_name, mode of the sem, permission, starting value);
+    track_semaphores[i] = sem_open(sem_name, O_CREAT, 0666, 1);
+    if (track_semaphores[i] == SEM_FAILED) {
+      perror("init_semaphore/sem_open");
+      exit(EXIT_FAILURE);
+    }
+  }
+}
+
 // Function for JOURNEY process to handle itineraries
 void journey_process() {
   printf("JOURNEY process started. Handling itineraries...\n");
@@ -77,17 +99,15 @@ void journey_process() {
     message.msg_type = train_data[i].train_id;
     memcpy(message.itinerary, train_data[i].route,
            sizeof(int) * train_data[i].route_length);
+    message.start = train_data[i].start;
+    message.dest = train_data[i].dest;
     message.num_steps = train_data[i].route_length;
 
     if (msgsnd(msgid, &message, sizeof(message) - sizeof(long), 0) == -1) {
       perror("msgsnd failed");
-      exit(1);
+      exit(EXIT_FAILURE);
     }
   }
-
-  // while (1) {
-  //   sleep(5); // Simulating work
-  // }
 }
 
 // Function for TRAIN_THR processes to move along the tracks
@@ -109,27 +129,88 @@ void train_process(int train_id) {
   }
 
   // Debug: Print received itinerary
-  printf("[DEBUG] Train %d received itinerary: ", train_id);
+  printf("[DEBUG] Train %d received itinerary: S%d ", train_id, message.start);
   for (int i = 0; i < message.num_steps; i++) {
     printf("MA%d ", message.itinerary[i]);
   }
-  printf("\n");
+  printf("S%d\n", message.dest);
 
-  for (int i = 0; i < message.num_steps; i++) {
-    printf("[DEBUG] Train %d moving to segment MA%d\n", train_id,
-           message.itinerary[i]);
+  // Open log file
+  char log_filename[20];
+  snprintf(log_filename, sizeof(log_filename), "T%d.log", train_id);
+  FILE *log = fopen(log_filename, "w");
+  if (log == NULL) {
+    perror("Failed opening file");
+    exit(EXIT_FAILURE);
+  }
+
+  int current_segment = -1;
+  for (int i = 0; i < message.num_steps; ++i) {
+    int next_segment = message.itinerary[i] - 1;
+    while (sem_trywait(track_semaphores[next_segment]) == -1) {
+      printf("Train %d waiting for segment MA%d\n", train_id,
+             message.itinerary[i]);
+      sleep(1);
+    }
+
+    // wait blocking call to block the semaphore
+    // implemented with the sem_trywait: since the specification
+    // required us to wait other 2 second if the segment is not
+    // available
+    if (current_segment != -1) {
+      // sem_post unlock the semaphore
+      sem_post(track_semaphores[current_segment]);
+    }
+    // Log entry
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    fprintf(log,
+            "[Current: %s%d], [Next: MA%d], %04d-%02d-%02d %02d:%02d:%02d\n",
+            (current_segment == -1) ? "S" : "MA",
+            (current_segment == -1) ? message.start : current_segment + 1,
+            next_segment + 1, t->tm_year + 1900, t->tm_mon + 1, t->tm_mday,
+            t->tm_hour, t->tm_min, t->tm_sec);
+
+    current_segment = next_segment;
+    // printf("[DEBUG] Train %d moving to segment MA%d\n", train_id,
+    //        message.itinerary[i]);
     sleep(2); // Simulating time taken to move
   }
+
+  // Log entry
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+  fprintf(log, "[Current: MA%d], [Next: S%d], %04d-%02d-%02d %02d:%02d:%02d\n",
+          current_segment + 1, message.dest, t->tm_year + 1900, t->tm_mon + 1,
+          t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
+
+  // unlock the last segment
+  sem_post(track_semaphores[current_segment]);
 
   printf("[DEBUG] Train %d reached its destination.\n", train_id);
   exit(EXIT_SUCCESS);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <MAP1|MAP2>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  // Open map file
+  // char map_file[20];
+  // snprintf(map_file, sizeof(map_file) - 1, "%s.txt", argv[1]);
+  // FILE *map = fopen(map_file, "w");
+  // if (map == NULL) {
+  //   perror("Failed opening file");
+  //   exit(EXIT_FAILURE);
+  // }
+
   printf("Starting CONTROLLER process...\n");
 
   // Initialize shared memory
   int *track_segments = init_shared_memory();
+  init_semaphores();
 
   // Fork JOURNEY process
   pid_t journey_pid = fork();
@@ -147,10 +228,22 @@ int main() {
     }
   }
 
+  int statuses[NUM_TRAINS];
+  for (int i = 0; i < NUM_TRAINS; i++) {
+    statuses[i] = -1;
+  }
   // Wait for all train processes to finish
   for (int i = 0; i < NUM_TRAINS; i++) {
     // why this wait(null) instead waitpid(train_pids[i])
-    wait(NULL);
+    waitpid(train_pids[i], &statuses[i], 0);
+    if (WIFEXITED(statuses[i])) {
+      statuses[i] = WEXITSTATUS(statuses[i]);
+    }
+  }
+
+  for (int i = 0; i < NUM_TRAINS; i++) {
+    printf("[DEBUG] Process %d wiht pid %d exited with status: %d\n", i,
+           train_pids[i], statuses[i]);
   }
 
   // Cleanup shared memory on exit
@@ -164,7 +257,7 @@ int main() {
 void load_routes(Train *trains) {
   FILE *file = fopen("MAP1.txt", "r");
   if (!file) {
-    perror("Failed to open MAP1.txt");
+    perror("Failed to open file");
     exit(1);
   }
 
